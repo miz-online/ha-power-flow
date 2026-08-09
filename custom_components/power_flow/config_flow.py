3d51dd7d8134ec958e32c69830ef73accb199616
Michael <miz@gmx.de>
2026-08-09 16:38:44 +0200

    fix

--- custom_components/power_flow/config_flow.py
+++ custom_components/power_flow/config_flow.py
@@ -4,6 +4,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
+from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 
 try:
